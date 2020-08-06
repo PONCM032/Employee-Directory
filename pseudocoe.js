9e5cc2fe1dcf55f8b1fedf19e739b851b9d90e51
Melanie <melanie-ponce@hotmail.com>
2020-08-05 21:49:23 -0400

    set up react

---
+++ pseudocoe.js
@@ -0,0 +1,25 @@
+//components of app
+    //app (set state in app - use render()  //state, what you typed in)
+        //header
+            //no state
+        //searchbar
+            //gonna have props
+        //table
+            //state, how data is sorted, list of employee can also live inside app
+            //staate, list of employees
+            //functionality of tbale
+                //map over employees, make an employee row for each one
+                //depending on column clicked, sort data appropriately
+                    //research how to sort array
+                    //sort array
+                    //set state of table
+                //searchbar text
+                    //filter the ees by some sort of criteria
+                    //does the string contain characters? research
+                        //make sure to maintain the original list
+            //searchrows
+                //each row is a component that is filling out the whole table
+                    //employee row - shows a single employee
+                //all columns
+                    //they themselves are their own components
+
