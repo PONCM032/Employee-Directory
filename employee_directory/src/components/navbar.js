15c080b24e01463a73f327fabfe4060fdaf8bf25
Melanie <melanie-ponce@hotmail.com>
2020-08-07 19:18:30 -0400

    add table and searchbar to app

---
+++ employee_directory/src/components/navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div className="container">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-light">
+        <h1>Employee Directory</h1>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
